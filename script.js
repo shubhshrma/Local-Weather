6076dffaf100b1788b8b56181fb38d6da8dbe74b
shubhshrma <shubham.31996@gmail.com>
2017-07-20 00:30:33 +0530

    Updated Files

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var state='c';
-
+var obj="";
 $(document).ready(function(){
 
     var html="";
@@ -10,20 +10,37 @@ $(document).ready(function(){
         var url="https://cors-anywhere.herokuapp.com/https://api.darksky.net/forecast/affb25aa15a43b5589b97f87e8663ae5/"+position.coords.latitude+","+position.coords.longitude;
         $.getJSON(url,function(json){
 
-            console.log(json);
-            rawText=JSON.stringify(json);
-            obj=JSON.parse(rawText);
-            document.getElementById("timezone").innerHTML = "<b>Timezone is " + obj.timezone+"</b>";
+
+            var rawText=JSON.stringify(json);
+            var obj=JSON.parse(rawText);
+            function temp()
+  {
+    if(state==='c')
+    {
+      document.getElementById("temperature").innerHTML ="<p>Temperature is " +obj.currently.temperature
+      +"&#8457;</p>";
+      state='f';
+    }
+    else {
+
+      document.getElementById("temperature").innerHTML ="<p>Temperature is " +(1/1.8*(obj.currently.temperature-32)).toFixed(2)
+      +"&#8451;</p>";
+      state='c';
+    }
+  }
+            $("#temperature").on("click",temp);
+            document.getElementById("timezone").innerHTML = "<p>Timezone is " + obj.timezone+"</p>";
             document.getElementById("timezone").style.fontSize="40px";
-            document.getElementById("temperature").innerHTML ="<b>Temperature is " +1/1.8*(obj.currently.temperature-32)
+            var skycons=new Skycons({"color":"#ADFF2F"});
+            skycons.add("icon",obj.currently.icon);
+            document.getElementById("conditions").innerHTML ='<p>Conditions : '+obj.currently.summary+'</p>';
+            skycons.play();
+            document.getElementById("conditions").style.fontSize="40px";
+            document.getElementById("temperature").innerHTML ="Temperature is " +(1/1.8*(obj.currently.temperature-32)).toFixed(2)
             +"&#8451;</b>";
             document.getElementById("temperature").style.fontSize="40px";
 
-            var skycons=new Skycons({"color":"#ADFF2F"});
-            skycons.add("icon",obj.currently.icon);
-            skycons.play();
-
-            document.getElementById("wind").innerHTML="<b>Wind speed is "+obj.currently.windSpeed+" mph</b>";
+            document.getElementById("wind").innerHTML="<p>Wind speed is "+obj.currently.windSpeed+" mph</p>";
             document.getElementById("wind").style.fontSize="40px";
         });
 
@@ -35,18 +52,5 @@ $(document).ready(function(){
   });
 
 
-  function temp()
-  {
-    if(state==='c')
-    {
-      document.getElementById("temperature").innerHTML ="<b>Temperature is " +obj.currently.temperature
-      +"&#8457;</b>";
-      state='f';
-    }
-    else {
 
-      document.getElementById("temperature").innerHTML ="<b>Temperature is " +1/1.8*(obj.currently.temperature-32)
-      +"&#8451;</b>";
-      state='c';
-    }
-  }
+ 
